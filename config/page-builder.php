e747f7ee400c683b8e106c101cd6e222de44cae1
Robert Rimoczi <robert@rimoczi.com>
2023-01-18 10:44:59 +0100

    update config

--- config/page-builder.php
+++ config/page-builder.php
@@ -1,7 +1,13 @@
 <?php
 
+use Dcodegroup\PageBuilder\Modules;
+
 return [
     'modules' => [
-        //
+        'Heading' => Modules\Heading::class,
+        'ImageSlider' => Modules\ImageSlider::class,
+        'SingleColumn' => Modules\SingleColumn::class,
+        'TwoColumn' => Modules\TwoColumn::class,
+        'TwoColumnWithImage' => Modules\TwoColumnWithImage::class,
     ],
 ];
