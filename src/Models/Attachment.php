8c3b0363c6fa1adf2bce5016e2a05b5da09b88cf
Ferenc Horvath <ferenc.horvath@sportmateclub.com>
2023-07-26 11:13:38 +0200

    Attachment extend UsesMedia

--- src/Models/Attachment.php
+++ src/Models/Attachment.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Attachment extends Model implements HasMedia
+class Attachment extends UsesMedia implements HasMedia
 {
     use InteractsWithMedia;
     use SoftDeletes;
