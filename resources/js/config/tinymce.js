66abfb6a85629a68534dd891ec709e4165bdc6ea
Ferenc Horvath <ferenc.horvath@sportmateclub.com>
2024-06-21 15:35:22 +0200

    Update rel list for tinyMCE

--- resources/js/config/tinymce.js
+++ resources/js/config/tinymce.js
@@ -1,8 +1,16 @@
 import { mediaGalleryPlugin } from '../media-gallery/tinymce.js';
 
 const config = {
-    plugins: "mediaGalleryPlugin",
-    toolbar: "mediaGalleryPlugin",
+    plugins: "mediaGalleryPlugin lists advlist autolink lists link image charmap preview anchor pagebreak",
+    toolbar: "mediaGalleryPlugin | undo redo | bold italic | alignleft aligncenter alignright | numlist bullist | image",
+    base_url: '/build/plugins/tinymce',
+    selector: 'textarea.wysiwyg',
+    images_file_types: 'jpg,svg,webp,png,jpeg,gif',
+    rel_list: [
+        { title: 'No Referrer', value: 'noreferrer' },
+        { title: 'No Follow', value: 'nofollow' },
+        { title: 'External Link', value: 'external' }
+    ],
     setup: function () {
         window.tinymce.PluginManager.add('mediaGalleryPlugin', mediaGalleryPlugin);
     }
