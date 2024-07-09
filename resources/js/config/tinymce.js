fa54af59658eebf09a527fa0c6f6ed9e08a14680
Ferenc Horvath <ferenc.horvath@sportmateclub.com>
2024-07-09 08:00:21 +0200

    remove base_url from config

--- resources/js/config/tinymce.js
+++ resources/js/config/tinymce.js
@@ -3,7 +3,6 @@ import { mediaGalleryPlugin } from '../media-gallery/tinymce.js';
 const config = {
     plugins: "mediaGalleryPlugin lists advlist autolink lists link image charmap preview anchor pagebreak",
     toolbar: "mediaGalleryPlugin | undo redo | bold italic | alignleft aligncenter alignright | numlist bullist | image",
-    base_url: '/build/plugins/tinymce',
     selector: 'textarea.wysiwyg',
     images_file_types: 'jpg,svg,webp,png,jpeg,gif',
     rel_list: [
